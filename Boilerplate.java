3a12f2173210cdc32c2167d23aeba0dc6db7900d
Shiv0166 <shivamrajpoot0616@gmail.com>
2025-09-13 21:56:06 +0530

    new

---
+++ Boilerplate.java
@@ -0,0 +1,32 @@
+// public class Boilerplate{
+//     public static void main(String args[]){
+//         System.out.print("Hello Shivam");
+//     }
+// }
+// public class Boilerplate{
+//     public static void main(String args[]){
+//         int a = 6;
+//         int b= 7;
+//         int sum = a+b;
+//         System.out.print(sum);
+//     }
+// }
+// import java.util.*;
+// public class Boilerplate{
+//     public static void main(String args[]){
+//         Scanner sc = new Scanner(System.in);
+//         boolean shiv = sc.nextBoolean();
+//         System.out.print(shiv);
+//     }
+// }
+
+import java.util.*;
+public class Boilerplate{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a+b;
+        System.out.print(sum);
+    }
+}
